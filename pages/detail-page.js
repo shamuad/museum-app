5886600e18bf0826edfcfab655063ffb101222bd
ARI DINAR <dinarari@gmail.com>
2019-01-08 14:35:52 +0100

    Add some codes

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,21 @@
 function submitComment() {
-    console.log("Test");
+    const inputField = document.getElementById('name');
+    const name = inputField.value;
+    console.log(name);
+    const textArea = document.getElementById('msg');
+    const msg = textArea.value;
+    console.log(msg);
+
+    const comment = document.createElement('section');
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p');
+
+    h3.innerHTML = `${name} said : `;
+    p.innerHTML = msg;
+    comment.classList.add(comment);
+    comment.appendChild(h3);
+    comment.appendChild(p);
+    console.log(comment);
+
 } 
+
