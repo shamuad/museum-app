5167ff8b3633375f22385d48c7e19c833d9e4a7c
ARI DINAR <dinarari@gmail.com>
2019-01-09 20:17:06 +0100

    Create elements with data from form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,5 +1,20 @@
 function submitComment() {
-    console.log("Hello");
+    const inputField = document.getElementById('name');
+    const name = inputField.value;
+    console.log(name);
+    const textArea = document.getElementById('msg');
+    const msg = textArea.value;
+    console.log(msg);
+    const comment = document.createElement('section');
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p');
+    
+    h3.innerHTML = `${name} said :`;
+    p.innerHTML = msg;
+    comment.classList.add('comment');
+    comment.appendChild(h3);
+    comment.appendChild(p);
+    console.log(comment);
 
 
     /* const inputField = document.getElementById('name');
