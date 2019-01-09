8b74e82444b4571646bdb0d1108701d3ffe41685
ARI DINAR <dinarari@gmail.com>
2019-01-09 15:11:23 +0100

    Detailed page changed

---
+++ pages/detail-page.js
@@ -0,0 +1,24 @@
+function submitComment() {
+    console.log("Hello");
+
+
+    /* const inputField = document.getElementById('name');
+    const name = inputField.value;
+    console.log(name);
+    const textArea = document.getElementById('msg');
+    const msg = textArea.value;
+    console.log(msg);
+
+    const comment = document.createElement('section');
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p');
+
+    h3.innerHTML = `${name} said : `;
+    p.innerHTML = msg;
+    comment.classList.add(comment);
+    comment.appendChild(h3);
+    comment.appendChild(p);
+    console.log(comment); */
+
+} 
+
