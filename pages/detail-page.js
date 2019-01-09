88a49aa0266cc80c1488ef1ab9bb5c53b85a004c
ARI DINAR <dinarari@gmail.com>
2019-01-09 20:27:25 +0100

    Display elements and reset form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,10 +1,8 @@
 function submitComment() {
     const inputField = document.getElementById('name');
     const name = inputField.value;
-    console.log(name);
     const textArea = document.getElementById('msg');
     const msg = textArea.value;
-    console.log(msg);
     const comment = document.createElement('section');
     const h3 = document.createElement('h3');
     const p = document.createElement('p');
@@ -14,26 +12,15 @@ function submitComment() {
     comment.classList.add('comment');
     comment.appendChild(h3);
     comment.appendChild(p);
-    console.log(comment);
+
+    const commentSection = document.getElementById('comments');
+    commentSection.appendChild(comment);
+
+    inputField.value = null;
+    textArea.value = null;
 
 
-    /* const inputField = document.getElementById('name');
-    const name = inputField.value;
-    console.log(name);
-    const textArea = document.getElementById('msg');
-    const msg = textArea.value;
-    console.log(msg);
 
-    const comment = document.createElement('section');
-    const h3 = document.createElement('h3');
-    const p = document.createElement('p');
-
-    h3.innerHTML = `${name} said : `;
-    p.innerHTML = msg;
-    comment.classList.add(comment);
-    comment.appendChild(h3);
-    comment.appendChild(p);
-    console.log(comment); */
 
 } 
 
