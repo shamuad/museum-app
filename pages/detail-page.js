f606eca5b865126f9b2c3e972272a4abb50da3ec
ARI DINAR <dinarari@gmail.com>
2019-01-10 16:41:04 +0100

    Set up js files and declare submitComment

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,13 +1,22 @@
 function submitComment() {
+    event.preventDefault()
     const inputField = document.getElementById('name');
-    const name = inputField.value;
     const textArea = document.getElementById('msg');
     const msg = textArea.value;
+    let name = inputField.value;
+
+
+    if (doesNotPassAllValidations(name, msg)) { // guard clause
+        return null;
+    }
+
     const comment = document.createElement('section');
     const h3 = document.createElement('h3');
     const p = document.createElement('p');
-    
-    h3.innerHTML = `${name} said :`;
+
+
+
+    h3.innerHTML = `${formatName(name)} said :`;
     p.innerHTML = msg;
     comment.classList.add('comment');
     comment.appendChild(h3);
@@ -18,9 +27,27 @@ function submitComment() {
 
     inputField.value = null;
     textArea.value = null;
+}
+function formatName(name){
+    if (name[0] !== name[0].toUpperCase()) {
+        return name.charAt(0).toUpperCase() + name.slice(1);
+    }
 
+    return name
+}
 
+function doesNotPassAllValidations(name, msg) {
+    
+    if (!name || !msg) {
+        console.log(msg, name)
+        alert('You forget to fill in your name or message')
+        return true;
+    }
 
+    if (msg.length > 280) {
+        alert('Your comment is too long');
+        return true;
+    }
 
-} 
-
+    return false;
+}
